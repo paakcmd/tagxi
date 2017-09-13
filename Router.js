6678ffdc93e803903d5af8ee62ea75dc513b7b4a
paakcmd <paakcmd@users.noreply.github.com>
2017-09-13 20:42:01 +0700

    app structure / router / reducer / action

--- Router.js
+++ Router.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Scene, Router, Actions } from 'react-native-router-flux'
-import LoginForm from './components/LoginForm'
-import EmployeeList from './components/EmployeeList'
-import EmployeeCreate from './components/EmployeeCreate'
-import EmployeeEdit from './components/EmployeeEdit'
+import LoginForm from './src/components/LoginForm'
+
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop:65 }}>
-      <Scene key="auth">
-        <Scene key ='login' component={LoginForm} title="Please Login" initial/>
-      </Scene>
-      <Scene key="main">
-        <Scene
-          onRight={() => Actions.employeeCreate()}
-          rightTitle="Add"
-          key='employeeList'
-          component={EmployeeList}
-          title="Employees"
-          initial
-        />
-        <Scene key='employeeCreate' component={EmployeeCreate} title="Create Employee" />
-        <Scene key='employeeEdit' component={EmployeeEdit} title="Edit Employee" />
-      </Scene>
+    <Scene key="auth">
+      <Scene key ='login' component={LoginForm} title="Please Login" initial/>
+    </Scene>
 
-    </Router>
+  </Router>
   )
 }
 
 export default RouterComponent;
+
+    // <Router sceneStyle={{ paddingTop:65 }}>
+    //   <Scene key="auth">
+    //     <Scene key ='login' component={LoginForm} title="Please Login" initial/>
+    //   </Scene>
+    //   <Scene key="main">
+    //     <Scene
+    //       onRight={() => Actions.employeeCreate()}
+    //       rightTitle="Add"
+    //       key='employeeList'
+    //       component={EmployeeList}
+    //       title="Employees"
+    //       initial
+    //     />
+    //     <Scene key='employeeCreate' component={EmployeeCreate} title="Create Employee" />
+    //     <Scene key='employeeEdit' component={EmployeeEdit} title="Edit Employee" />
+    //   </Scene>
+
+    // </Router>
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            Hello
+        )
+    }
+}
+
+export default LoginForm
